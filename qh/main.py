118e90a216ddb1db5525fbadc8ab3653ebee731f
Thor Whalen <thorwhalen1@gmail.com>
2023-11-23 13:20:54 +0100

    fix: name changes in py2http

--- qh/main.py
+++ qh/main.py
@@ -1,6 +1,6 @@
 from typing import Iterable, Callable
 
-from py2http import mk_http_service
+from py2http import mk_app
 
 from qh.util import flat_callable_for
 
@@ -9,8 +9,9 @@ def _ascertain_flat_func_list(funcs):
     if isinstance(funcs, Callable):
         return _ascertain_flat_func_list([funcs])
     else:
-        assert isinstance(funcs, Iterable) and all(map(callable, funcs)), \
-            f"funcs is supposed to be a callable or an iterable of callables: {funcs}"
+        assert isinstance(funcs, Iterable) and all(
+            map(callable, funcs)
+        ), f"funcs is supposed to be a callable or an iterable of callables: {funcs}"
         return [flat_callable_for(func) for func in funcs]
 
 
@@ -19,5 +20,4 @@ def mk_http_service_app(funcs, input_trans=None):
     if input_trans:
         for func in funcs:
             func.input_mapper = input_trans
-    return mk_http_service(funcs)
-
+    return mk_app(funcs)
